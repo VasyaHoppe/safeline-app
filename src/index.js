62270740452a7b9f3f45c68fd37a3c422282271c
Darya Zankevich <vererohoppe@mail.ru>
2018-05-27 00:55:44 +0300

    Add correct footer + wrapper + added loading templates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import 'babel-polyfill';
 import 'whatwg-fetch';
 
-function * lol() {
+const routes = {
+    '': 'templates/main.html',
+    'main': 'templates/main.html',
+    'services': 'templates/main.html',
+    'licenses': 'templates/licenses.html',
+    'contacts': 'templates/main.html',
+    'about': 'templates/main.html'
+};
 
+const params = { 
+    method: 'GET',
+    headers: new Headers(),
+    mode: 'cors',
+    cache: 'default'
+};
+
+const loadByRoute = ({ location: { hash = '' } }) => {
+    const route = hash.substr(2, (hash.length - 1));
+    return fetch(routes[route])
+        .then(res => res.text())
+        .then(html => {
+            console.log(html);
+        });
 }
+
+window.addEventListener("hashchange", ({ target }) => loadByRoute(target), false);
