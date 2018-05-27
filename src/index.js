7c268a7a6f6cbb0529072610dfaf94d00b6ae444
Darya Zankevich <vererohoppe@mail.ru>
2018-05-27 23:24:51 +0300

    Main changes: 1) Add spinner 2) Add content loading 3) Changed tab 4) Reduce wrapper size

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
 import 'babel-polyfill';
 import 'whatwg-fetch';
 
+let spinnerHtml;
+
 const routes = {
     '': 'templates/main.html',
     'main': 'templates/main.html',
     'services': 'templates/main.html',
     'licenses': 'templates/licenses.html',
     'contacts': 'templates/main.html',
-    'about': 'templates/main.html'
+    'about': 'templates/main.html',
 };
 
-const params = { 
-    method: 'GET',
-    headers: new Headers(),
-    mode: 'cors',
-    cache: 'default'
-};
+const mainContentDiv = document.querySelector('#mainContent');
 
-const loadByRoute = ({ location: { hash = '' } }) => {
-    const route = hash.substr(2, (hash.length - 1));
-    return fetch(routes[route])
+const sleeper = ms => x => new Promise(resolve => setTimeout(() => resolve(x), ms));
+const loadToMain = content => mainContentDiv.innerHTML = content;
+
+const loadTemplate = (url = '') => loadSpinner()
+    .then(sleeper(2000))
+    .then(() => fetch(url))    
+    .then(res => res.text())
+    .then(html => loadToMain(html));
+
+const loadSpinner = async () => {
+    if (spinnerHtml) {        
+        return loadToMain(spinnerHtml);
+    }
+    return fetch('templates/spinner.html')
         .then(res => res.text())
-        .then(html => {
-            console.log(html);
-        });
-}
+        .then(html => loadToMain(spinnerHtml = html));        
+};
 
+const loadByRoute =
+    ({ location: { hash = '' } }) => loadTemplate(routes[hash.substr(2, (hash.length - 1))]);
+
+// After init application, need to load main page
+loadTemplate(routes['']);
 window.addEventListener("hashchange", ({ target }) => loadByRoute(target), false);
